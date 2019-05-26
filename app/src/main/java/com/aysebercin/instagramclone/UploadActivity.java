655f4d31e5c67d2c2151b08e7a2cd16e73977a4a
ayse <bercin.ayse@gmail.com>
2019-05-27 01:26:58 +0300

    completed

--- app/src/main/java/com/aysebercin/instagramclone/UploadActivity.java
+++ app/src/main/java/com/aysebercin/instagramclone/UploadActivity.java
@@ -14,41 +14,42 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class UploadActivity extends AppCompatActivity {
 
-    EditText commentText;
+    EditText commentText ;
     ImageView imageView;
     Bitmap chosenImage;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
 
-       commentText = findViewById(R.id.commentText);
-       imageView = findViewById(R.id.imageView);
+        commentText = findViewById(R.id.commentText);
+        imageView = findViewById(R.id.imageView);
     }
 
     public void chooseImage(View view){
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},2);
-        } else {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},2);
+        }else{
             Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(intent,1);
         }
-
-    }
-    public void Upload(View view){
-
-
 
     }
 
@@ -56,8 +57,8 @@ public class UploadActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
-        if (requestCode == 2) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if(requestCode == 2){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent,1);
             }
@@ -66,14 +67,11 @@ public class UploadActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
-        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
-
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null){
             Uri uri = data.getData();
-
             try {
                 chosenImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
                 imageView.setImageBitmap(chosenImage);
@@ -81,10 +79,44 @@ public class UploadActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-
         }
-
 
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    public void Upload(View view){
+
+        String comment  = commentText.getText().toString();
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        chosenImage.compress(Bitmap.CompressFormat.PNG,50,byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+
+        ParseFile parseFile = new ParseFile("image.png",bytes);
+
+
+        ParseObject parseObject = new ParseObject("Posts");
+        parseObject.put("image",parseFile);
+        parseObject.put("comment",comment);
+        parseObject.put("username",ParseUser.getCurrentUser().getUsername());
+        parseObject.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+
+                if(e != null){
+                    Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Post Uploaded!!",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(),FeedActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+
+
+    }
+
+
 }
